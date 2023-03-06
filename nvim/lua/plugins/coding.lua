07cf0c27fa6868694ecfd276d561e50ce726710c
Minh Phung <phung.q.minh@gmail.com>
2023-03-06 22:26:49 +0700

    fix(nvim/coding): correct require

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -52,7 +52,7 @@ return {
     config = function(_, opts)
       require("mini.ai").setup(opts)
       -- register all text objects with which-key
-      if require("lazyvim.util").has("which-key.nvim") then
+      if require("util").has("which-key.nvim") then
         ---@type table<string, string|table>
         local i = {
           [" "] = "Whitespace",
