5ed8aaf8f2cac0ad4a7260bc1c4b6fe559003fa4
Minh Phung <phung.q.minh@gmail.com>
2023-09-12 17:18:21 +0700

    feat(terminals): light font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -158,7 +158,7 @@ return {
   font = wezterm.font_with_fallback({
     {
       family = 'JetBrainsMono Nerd Font',
-      weight = 'Light'
+      weight = 'ExtraLight'
     }
   }),
   font_size = 16,
