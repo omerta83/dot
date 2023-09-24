90f93025744edccba2f97aa61b23bc7e8499a07e
Minh Phung <phung.q.minh@gmail.com>
2023-09-24 16:23:57 +0700

    fix(nvim): <Tab> is now only for copilot and snippets

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -6,14 +6,14 @@ return {
     event = "InsertEnter",
     opts = {
       suggestion = {
-        enabled = false,
-        -- auto_trigger = true,
-        -- keymap = {
-        --   accept = false
-        -- }
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = false,
+        }
       },
       panel = {
-        enabled = false
+        enabled = true,
       }
     }
   },
@@ -65,32 +65,18 @@ return {
           })
         end
       },
-      {
-        "zbirenbaum/copilot-cmp",
-        config = function()
-          require("copilot_cmp").setup({
-            event = { "InsertEnter", "LspAttach" },
-            fix_pairs = true,
-          })
-        end
-      },
     },
     config = function()
       local cmp = require('cmp')
+      local defaults = require("cmp.config.default")()
       local luasnip = require('luasnip')
 
       -- If you want insert `(` after select function or method item
-      -- local autopairs = require('nvim-autopairs.completion.cmp')
-      -- cmp.event:on(
-      --   'confirm_done',
-      --   autopairs.on_confirm_done()
-      -- )
-
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
+      local autopairs = require('nvim-autopairs.completion.cmp')
+      cmp.event:on(
+        'confirm_done',
+        autopairs.on_confirm_done()
+      )
 
       cmp.setup({
         snippet = {
@@ -98,39 +84,7 @@ return {
             require("luasnip").lsp_expand(args.body)
           end,
         },
-        sorting = {
-          priority_weight = 2,
-          comparators = {
-            -- cmp.config.compare.exact,
-            require("copilot_cmp.comparators").prioritize,
-
-            -- Extract from TJ's config: https://github.com/tjdevries/config_manager/blob/78608334a7803a0de1a08a9a4bd1b03ad2a5eb11/xdg_config/nvim/after/plugin/completion.lua#L129
-            cmp.config.compare.offset,
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            cmp.config.compare.recently_used,
-
-            -- copied from cmp-under, but I don't think I need the plugin for this.
-            -- I might add some more of my own.
-            function(entry1, entry2)
-              local _, entry1_under = entry1.completion_item.label:find "^_+"
-              local _, entry2_under = entry2.completion_item.label:find "^_+"
-              entry1_under = entry1_under or 0
-              entry2_under = entry2_under or 0
-              if entry1_under > entry2_under then
-                return false
-              elseif entry1_under < entry2_under then
-                return true
-              end
-            end,
-
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
-          },
-        },
-        -- sorting = defaults.sorting,
+        sorting = defaults.sorting,
         mapping = cmp.mapping.preset.insert({
           ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
@@ -144,20 +98,16 @@ return {
             select = true
           }),
           ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
+            if require("copilot.suggestion").is_visible() then
+              require("copilot.suggestion").accept()
+            elseif luasnip.jumpable(1) then
+              luasnip.jump(1)
             else
               fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
             end
           end, { "i", "s" }),
           ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
+            if luasnip.jumpable(-1) then
               luasnip.jump(-1)
             else
               fallback()
@@ -167,7 +117,8 @@ return {
         sources = cmp.config.sources({
           -- Copilot Source
           { name = 'nvim_lsp', group_index = 1 },
-          { name = "copilot",  group_index = 2 },
+          -- { name = "copilot",  group_index = 2 },
+          { name = "luasnip",  group_index = 2 },
           { name = 'path',     group_index = 2 },
           {
             name = 'buffer',
@@ -178,7 +129,6 @@ return {
             },
             group_index = 2
           },
-          { name = "luasnip", group_index = 2 },
         }),
         formatting = {
           format = function(entry, vim_item)
@@ -199,7 +149,6 @@ return {
             hl_group = "CmpGhostText",
           },
         },
-        preselect = cmp.PreselectMode.None,
         completion = {
           completeopt = "menu,menuone,noselect",
         },
