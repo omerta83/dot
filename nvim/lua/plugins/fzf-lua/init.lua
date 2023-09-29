0fe3f61f4fcb3ced416e082fa324f6eb9eb50da1
Minh Phung <phung.q.minh@gmail.com>
2023-09-29 15:28:09 +0700

    feat(nvim): fzf to show man and helps

--- nvim/lua/plugins/fzf-lua/init.lua
+++ nvim/lua/plugins/fzf-lua/init.lua
@@ -4,9 +4,6 @@ local FilesPicker = {
   opts = nil,
   ignoring = nil,
 }
-local fzf_opts = {
-  ['--info'] = 'inline',
-}
 FilesPicker.toggle = function(_, _)
   FilesPicker.pick(FilesPicker.opts)
 end
@@ -18,7 +15,6 @@ FilesPicker.pick = function(opts)
   opts.actions = {
     ['ctrl-g'] = FilesPicker.toggle,
   }
-  opts.fzf_opts = fzf_opts
   local behavior = ''
   if FilesPicker.ignoring then
     behavior = 'respecting'
@@ -49,6 +45,8 @@ return {
     { "<leader>f;",      "<cmd>FzfLua resume<CR>",                    desc = "Resume (FzfLua)" },
     { "<leader>fc",      "<cmd>FzfLua commands<CR>",                  desc = "Commands (FzfLua)" },
     { "<leader>fh",      "<cmd>FzfLua command_history<CR>",           desc = "Command history (FzfLua)" },
+    { "<leader>fm",      "<cmd>FzfLua man_pages<CR>",                 desc = "Man page (FzfLua)" },
+    { "<leader>f?",      "<cmd>FzfLua help_tags<CR>",                 desc = "Help tags (FzfLua)" },
     { "<leader>fo",      "<cmd>FzfLua lsp_document_symbols<CR>",      desc = "Document symbols (FzfLua)" },
     { "<leader>fO",      "<cmd>FzfLua lsp_workspace_symbols<CR>",     desc = "Workspace symbols (FzfLua)" },
     { "<leader>fa",      "<cmd>FzfLua lsp_code_actions<CR>",          desc = "Code actions (FzfLua)" },
@@ -62,112 +60,153 @@ return {
   opts = function()
     local actions = require "fzf-lua.actions"
     local preview_pager = vim.fn.executable("delta") == 1 and "delta --width=$FZF_PREVIEW_COLUMNS"
+    local icons = require('config.icons')
+
     return {
       -- for performance
-      global_git_icons = false,
-      global_file_icons = false,
-      fzf_opts = fzf_opts,
-      keymap = {
+      global_git_icons  = false,
+      -- global_file_icons = false,
+      file_icon_padding = ' ',
+      fzf_opts          = {
+        ['--info'] = 'default',
+        ['--pointer'] = icons.misc.Arrow,
+        ['--marker'] = '✓',
+      },
+      keymap            = {
         builtin = {
-          ['<A-/>'] = 'toggle-help',
-          ['<C-a>'] = 'toggle-preview',
-          ['<C-f>'] = 'preview-page-down',
-          ['<C-b>'] = 'preview-page-up',
+          -- neovim `:tmap` mappings for the fzf win
+          ["<A-/>"] = "toggle-help",
+          ["<F2>"]  = "toggle-fullscreen",
+          -- Only valid with the 'builtin' previewer
+          ["<F3>"]  = "toggle-preview-wrap",
+          ["<C-y>"] = "toggle-preview",
+          -- Rotate preview clockwise/counter-clockwise
+          -- ["<F5>"]     = "toggle-preview-ccw",
+          -- ["<F6>"]     = "toggle-preview-cw",
+          -- ["<C-f>"]    = "preview-page-down",
+          -- ["<C-b>"]    = "preview-page-up",
+          -- ["<S-left>"] = "preview-page-reset",
         },
         fzf = {
-          ['alt-s'] = 'toggle',
-          ['alt-a'] = 'toggle-all',
-          ['ctrl-a'] = 'toggle-preview'
+          -- fzf '--bind=' options
+          ["ctrl-z"] = "abort",
+          ["ctrl-u"] = "unix-line-discard",
+          ["ctrl-f"] = "preview-page-down",
+          ["ctrl-b"] = "preview-page-up",
+          ["ctrl-a"] = "beginning-of-line",
+          ["ctrl-e"] = "end-of-line",
+          ["ctrl-s"] = "jump-accept",
+          ["alt-a"]  = "toggle-all",
+          ["ctrl-l"] = "clear-selection",
+          -- Only valid with fzf previewers (bat/cat/git/etc)
+          ["f3"]     = "toggle-preview-wrap",
+          ["ctrl-y"] = "toggle-preview",
         },
       },
-      actions = {
-        -- These override the default tables completely
-        -- no need to set to `false` to disable an action
-        -- delete or modify is sufficient
+      actions           = {
         files = {
-          -- providers that inherit these actions:
-          --   files, git_files, git_status, grep, lsp
-          --   oldfiles, quickfix, loclist, tags, btags
-          --   args
-          -- default action opens a single selection
-          -- or sends multiple selection to quickfix
-          -- replace the default action with the below
-          -- to open all files whether single or multiple
           -- ["default"]     = actions.file_edit,
           ["default"] = actions.file_edit_or_qf,
-          ["ctrl-s"]  = actions.file_split,
+          ["ctrl-x"]  = actions.file_split,
           ["ctrl-v"]  = actions.file_vsplit,
           ["ctrl-t"]  = actions.file_tabedit,
           ["ctrl-q"]  = actions.file_sel_to_qf,
-          ["ctrl-l"]  = actions.file_sel_to_ll,
+          ["alt-l"]   = actions.file_sel_to_ll,
         },
         buffers = {
-          -- providers that inherit these actions:
-          --   buffers, tabs, lines, blines
           ["default"] = actions.buf_edit,
-          ["ctrl-s"]  = actions.buf_split,
+          -- ["ctrl-x"]  = actions.buf_split,
           ["ctrl-v"]  = actions.buf_vsplit,
           ["ctrl-t"]  = actions.buf_tabedit,
         }
       },
-      previewers = {
+      previewers        = {
         git_diff = {
           pager = preview_pager
-        }
+        },
+        man = {
+          -- NOTE: remove the `-c` flag when using man-db
+          -- replace with `man -P cat %s | col -bx` on OSX
+          cmd = "man -P bat %s | col -bx",
+        },
       },
-      winopts = {
-        height = 0.7,
-        width = 0.55,
+      winopts           = {
+        height = 0.75,
+        width = 0.5,
         preview = {
+          -- defer the execution of bat to fzf
+          default = 'bat_native',
           scrollbar = false,
+          delay = 100,
           hidden = 'hidden',
           layout = 'vertical',
-          -- vertical = 'down:45%',
+          vertical = 'down:45%',
         },
       },
       -- Configuration for specific commands.
-      files = {
-        winopts = {
-          preview = { hidden = 'hidden' },
-        },
+      files             = {
+        cwd_prompt = false,
+        prompt = 'Files❯ ',
       },
-      git = {
+      git               = {
         status = {
+          prompt = 'Git Status❯ ',
+          winopts = {
+            preview = { hidden = 'nohidden' },
+          },
           actions = {
             ["right"]  = false,
             ["left"]   = false,
-            -- ["ctrl-l"] = { fn = actions.git_unstage, reload = true },
-            -- ["ctrl-h"] = { fn = actions.git_stage, reload = true },
-            ["ctrl-s"] = { fn = actions.git_stage_unstage, reload = true },
+            ["ctrl-x"] = actions.file_split, -- remap reset key
+            ["ctrl-h"] = { fn = actions.git_stage_unstage, reload = true },
           }
         },
         commits = {
+          prompt = 'Git Commits❯ ',
+
           preview_pager = preview_pager,
         },
         bcommits = {
+          prompt = 'Buffer Commits❯ ',
           preview_pager = preview_pager,
         }
       },
-      grep = {
-        header_prefix = require('config.icons').misc.Search .. ' ',
-        fzf_opts = fzf_opts,
+      grep              = {
+        prompt = 'Grep❯ ',
+        header_prefix = icons.misc.Search .. ' ',
       },
-      lsp = {
+      lsp               = {
+        prompt_postfix = '❯ ',
         cwd_only = true,
         symbols = {
-          symbol_icons = require('config.icons').kinds,
+          prompt_postfix = '❯ ',
+          symbol_icons = icons.kinds,
         },
+        code_actions = {
+          prompt = 'Code Actions❯ ',
+        }
       },
-      oldfiles = {
+      diagnostics       = {
+        prompt = 'Diagnostics❯ ',
+      },
+      oldfiles          = {
+        prompt = 'Recent Files❯ ',
         cwd_only = true,
         include_current_session = true,
         winopts = {
           preview = { hidden = 'hidden' },
         },
       },
-      buffers = {
+      buffers           = {
+        prompt = 'Buffers❯ ',
         cwd_only = true,
-      }
+        actions = {
+          ["ctrl-x"] = actions.buf_split,
+          ["ctrl-d"] = { fn = actions.buf_del, reload = true },
+        }
+      },
+      manpages          = { previewer = "man_native" },
+      helptags          = { previewer = "help_native" },
     }
   end,
   config = function(_, opts)
