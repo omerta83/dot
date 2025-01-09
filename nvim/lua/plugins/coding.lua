04edb1cb370d065fe350be2e83515698f8265333
Minh Phung <phung.q.minh@gmail.com>
2025-01-09 11:43:42 +0700

    feat(nvim): Make Comments context-awareness

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -26,26 +26,26 @@ return {
   -- comment
   {
     'numToStr/Comment.nvim',
-    event = { "BufReadPost", "BufNewFile" },
-    -- event = "VeryLazy",
+    -- event = { "BufReadPost", "BufNewFile" },
+    event = "VeryLazy",
     dependencies = {
-      -- 'JoosepAlviste/nvim-ts-context-commentstring',
-      -- init = function ()
-      --   vim.g.skip_ts_context_commentstring_module = true
-      -- end,
+      'JoosepAlviste/nvim-ts-context-commentstring',
+      init = function ()
+        vim.g.skip_ts_context_commentstring_module = true
+      end,
     },
     config = function()
       require('Comment').setup({
-        -- pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
       })
     end
   },
-  {
-    'folke/ts-comments.nvim',
-    opts = {},
-    event = 'VeryLazy',
-    enabled = vim.fn.has('nvim-0.10.0') == 1,
-  },
+  -- {
+  --   'folke/ts-comments.nvim',
+  --   opts = {},
+  --   event = 'VeryLazy',
+  --   enabled = vim.fn.has('nvim-0.10.0') == 1,
+  -- },
 
   {
     'echasnovski/mini.ai',
@@ -93,16 +93,18 @@ return {
   --- Generate docs
   {
     "danymat/neogen",
+    cmd = "Neogen",
+    dependencies = 'echasnovski/mini.snippets',
     keys = {
       {
         "<leader>cc",
         function()
           require("neogen").generate({})
         end,
-        desc = "Neogen Comment",
+        desc = "Generate [c]omment",
       },
     },
-    opts = { snippet_engine = "luasnip" },
+    opts = { snippet_engine = "mini" },
   },
 
   --- refactoring
@@ -110,48 +112,48 @@ return {
     "ThePrimeagen/refactoring.nvim",
     keys = {
       {
-        "<leader>rr",
+        "<leader>cxr",
         function()
           require("refactoring").select_refactor()
         end,
         mode = { "n", "x" },
-        desc = "Refactor Selector"
+        desc = "Refacto[r] selector"
       },
       {
-        "<leader>re",
+        "<leader>cxx",
         function() require('refactoring').refactor('Extract Function') end,
         mode = "x",
-        desc = "Extract Function"
+        desc = "E[x]tract function"
       },
       {
-        "<leader>rf",
+        "<leader>cxf",
         function() require('refactoring').refactor('Extract Function To File') end,
         mode = "x",
-        desc = "Extract Function To File",
+        desc = "Extract function to [f]ile",
       },
       {
-        "<leader>rv",
+        "<leader>cxv",
         function() require('refactoring').refactor('Extract Variable') end,
         mode = "x",
-        desc = "Extract Variable",
+        desc = "Extract [v]ariable",
       },
       {
-        "<leader>ri",
+        "<leader>cxi",
         function() require('refactoring').refactor('Inline Variable') end,
         mode = { "n", "x" },
-        desc = "Inline Variable",
+        desc = "Extract [i]nline variable",
       },
       {
-        "<leader>rb",
+        "<leader>cxb",
         function() require('refactoring').refactor('Extract Block') end,
         mode = "n",
-        desc = "Extract Block",
+        desc = "Extract [b]lock",
       },
       {
-        "<leader>rB",
+        "<leader>cxB",
         function() require('refactoring').refactor('Extract Block To File') end,
         mode = "n",
-        desc = "Extract Block To File",
+        desc = "Extract [B]lock to file",
       }
     },
     opts = {},
@@ -194,51 +196,67 @@ return {
   },
 
   -- -- Navigating
-  -- {
-  --   "folke/flash.nvim",
-  --   event = "VeryLazy",
-  --   -- event = { "BufReadPost", "BufNewFile" },
-  --   opts = {
-  --     labels = "0123456789", -- prevent messing with inserting characters
-  --     search = {
-  --       multi_window = false,
-  --     },
-  --     modes = {
-  --       char = {
-  --         autohide = true,
-  --         jump_labels = true,
-  --         -- label = { exclude = "hjkliardcx" },
-  --         multi_line = true,
-  --       }
-  --     },
-  --   },
-  --   keys = {
-  --     {
-  --       "s",
-  --       mode = { "n", "o", "x" },
-  --       function() require("flash").jump() end,
-  --       desc = "Flash",
-  --     },
-  --     {
-  --       "<leader>S",
-  --       mode = { "n", "o", "x" },
-  --       function() require("flash").treesitter() end,
-  --       desc = "Flash Treesitter",
-  --     },
-  --     {
-  --       "r",
-  --       mode = "o",
-  --       function() require("flash").remote() end,
-  --       desc = "Remote Flash",
-  --     },
-  --     {
-  --       "R",
-  --       mode = { "o", "x" },
-  --       function() require("flash").treesitter_search() end,
-  --       desc = "Treesitter Search",
-  --     },
-  --   }
-  -- },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    -- event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      -- labels = "0123456789", -- prevent messing with inserting characters for / and ?
+      search = {
+        multi_window = false,
+      },
+      jump = {
+        -- automatically jump when there is only one match
+        autojump = true,
+      },
+      modes = {
+        -- options used when flash is activated through
+        -- a regular search with `/` or `?`
+        search = {
+          enabled = true,
+        },
+        -- options used when flash is activated through
+        -- `f`, `F`, `t`, `T`, `;` and `,` motions
+        char = {
+          autohide = true,
+          jump_labels = true,
+          multi_line = false,
+          jump = {
+            -- hide labels after jump if only one match
+            autojump = true,
+          },
+        },
+      },
+    },
+    keys = {
+      {
+        "s",
+        mode = { "n", "o", "x" },
+        function() require("flash").jump() end,
+        desc = "Flash",
+      },
+      -- use a jump label, or use ; and , to increase/decrease the selection
+      -- S can be chained to increase the selection
+      {
+        "S",
+        mode = { "n", "x", "o" },
+        function() require("flash").treesitter() end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function() require("flash").remote() end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc = "Treesitter Search",
+      },
+    }
+  },
 
   -- surround
   {
